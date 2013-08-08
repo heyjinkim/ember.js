6be79f15c4fb078ca0a6a3942b87cde293808698
selvaganesh.b <selvaganesh.b@zohocorp.com>
2013-08-08 17:01:44 +0530

    Em.Components typo fix - fixes #3118

--- packages/ember-views/lib/views/component.js
+++ packages/ember-views/lib/views/component.js
@@ -156,7 +156,7 @@ Ember.Component = Ember.View.extend(Ember.TargetActionSupport, {
 
     ```handlebars
     {{! categories.hbs}}
-    {{my-tree didClickTreeNode=didClickCategory}}
+    {{my-tree didClickTreeNode='didClickCategory'}}
     ```
 
     @method sendAction
